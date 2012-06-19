e69adcade0d91ea7d3ba5d55de5810f2032e18e3
Ian Morgan <ian.j.morgan@gmail.com>
2012-06-19 23:51:38 +0100

    cleanup ConfigManger code

--- core/src/main/java/com/lateblindcat/sid/core/framework/ConfigManager.java
+++ core/src/main/java/com/lateblindcat/sid/core/framework/ConfigManager.java
@@ -9,52 +9,40 @@ import java.util.List;
 import java.util.Properties;
 
 public class ConfigManager {
-	
-	public void loadModuleConfigs() throws Exception {
-		//see http://bill.burkecentral.com/2008/01/14/scanning-java-annotations-at-runtime/
-		
-		ClassLoader cl = Thread.currentThread().getContextClassLoader();
-		Enumeration<URL> urls = cl.getResources("META-INF/bootstrap.properties");
-		
-		
-		
-		//URLClassLoader loader = new URLClassLoader(new URL[0]);
-		List<URL> urlsToLoad = new ArrayList<URL>();
-		while (urls.hasMoreElements()){
-			URL url = urls.nextElement(); 
-			String path =  url.toString();
 
-			System.out.println (path);
-	
-			//if (path.startsWith("file:")) {
-				path = path.substring(0,path.length()-20);
-	
-				urlsToLoad.add(new URL(path));
-							
-			//}
-			
+	private static final String BOOTSTRAP = "bootstrap.properties";
+
+	public void loadModuleConfigs() throws Exception {
+		// see
+		// http://bill.burkecentral.com/2008/01/14/scanning-java-annotations-at-runtime/
+
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		Enumeration<URL> urls = cl.getResources("META-INF/" + BOOTSTRAP);
+
+		List<URL> urlsToLoad = new ArrayList<URL>();
+		while (urls.hasMoreElements()) {
+			URL url = urls.nextElement();
+			String path = url.toString();
+			path = path.substring(0, path.length() - BOOTSTRAP.length());
+			urlsToLoad.add(new URL(path));
+
 		}
 		URLClassLoader loader = new URLClassLoader(urlsToLoad.toArray(new URL[0]));
-		
-		BufferedInputStream bis = new BufferedInputStream (loader.getResourceAsStream("bootstrap.properties"));
+
+		BufferedInputStream bis = new BufferedInputStream(loader.getResourceAsStream(BOOTSTRAP));
 
 		byte data[] = new byte[1000];
 		bis.read(data);
-		
-		String result = new String(data);
-		
-		System.out.println(result);
-		
+
+		//String result = new String(data);
+
+
 		Properties prop = new Properties();
-		prop.load(loader.getResourceAsStream("bootstrap.properties"));
-		
-		System.out.println (prop.getProperty("configClass"));
-		
+		prop.load(loader.getResourceAsStream(BOOTSTRAP));
+
 		Class<?> clazz = this.getClass().getClassLoader().loadClass(prop.getProperty("configClass"));
-		
-		ModuleConfig o = (ModuleConfig)clazz.newInstance();
-		
-		System.out.println (o.name());
+		ModuleConfig o = (ModuleConfig) clazz.newInstance();
+
 	}
 
 }
